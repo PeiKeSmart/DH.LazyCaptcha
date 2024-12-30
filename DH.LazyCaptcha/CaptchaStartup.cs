d09c95cca6eb13150633ebba4a25a309d235f84e
猿人易 <qcjxberin@outlook.com>
2024-12-30 16:22:34 +0800

    更新 Pek.VirtualFileSystem 包版本
    更新了 `Pek.VirtualFileSystem` 包的版本，从 `4.0.2024.1226-beta1151` 更新到 `4.0.2024.1230-beta0335`。

--- DH.LazyCaptcha/CaptchaStartup.cs
+++ DH.LazyCaptcha/CaptchaStartup.cs
@@ -38,4 +38,9 @@ public class CaptchaStartup : IDHStartup
     /// 获取此启动配置实现的顺序
     /// </summary>
     public Int32 StartupOrder => 102; //常见服务应在错误处理程序之后加载
+
+    /// <summary>
+    /// 获取此启动配置实现的顺序。主要针对ConfigureMiddleware、UseRouting前执行的数据、UseAuthentication或者UseAuthorization后面 Endpoints前执行的数据
+    /// </summary>
+    public Int32 ConfigureOrder => 200;
 }
